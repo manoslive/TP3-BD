c1439fa5ca982095153b5a36b6dc505b21c50a94
ChuckCooper <shaun.11.cooper@gmail.com>
2015-04-20 22:41:37 -0400

    ooo lala

---
+++ src/PkgEmployesClg/Menu.java
@@ -0,0 +1,16 @@
+package PkgEmployesClg;
+import javax.swing.*;
+
+
+
+/**
+ * Created by shaun on 2015-04-20.
+ */
+public class Menu {
+    private JButton BTN_ActionAdherent;
+    private JButton BTN_ConsulterLivres;
+    private JButton AjouterPret;
+    private JButton ConsulterLivresPrets;
+    private JButton BTN_RechercheLivre;
+    private JButton BTN_LivrePlusEmpruntes;
+}
