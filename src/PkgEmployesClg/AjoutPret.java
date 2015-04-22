799e020c178a0bad1eee52f8f4aebd293a9ceab8
ChuckCooper <shaun.11.cooper@gmail.com>
2015-04-22 00:13:29 -0400

    YEAHHH MOTHA FUCKKAA

--- src/PkgEmployesClg/AjoutPret.java
+++ src/PkgEmployesClg/AjoutPret.java
@@ -1,7 +1,9 @@
 package PkgEmployesClg;
 
 import javax.swing.*;
-import java.sql.Connection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 /**
  * Created by shaun on 2015-04-21.
@@ -14,8 +16,31 @@ public class AjoutPret {
     private JButton BTN_Ajouter;
     public JPanel panel1;
     Connection connection;
+    ResultSet rset;
     public AjoutPret(Connection conn)
     {
         connection = conn;
+        BTN_Ajouter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    CallableStatement stm = connection.prepareCall("{call BIBLIOTHEQUE.AJOUTPRET(?,?,?,?)}");
+                    //stm.registerOutParameter(1, OracleTypes.CURSOR);
+                    stm.setLong(1, Long.getLong(TB_NumExemplaire.getText()));
+                    stm.setLong(2, Long.getLong(TB_NumExemplaire.getText()));
+                    stm.setLong(3, Long.getLong(TB_NumExemplaire.getText()));
+                    stm.setLong(4, Long.getLong(TB_NumExemplaire.getText()));
+                    stm.execute(); //execution de la fonction
+                    // Caster le paramètre de retour en ResultSet
+                    rset = (ResultSet) stm.getObject(1);
+
+                    ResultSetMetaData metaData = rset.getMetaData();
+
+                    while (rset.next()) {}
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
     }
 }
